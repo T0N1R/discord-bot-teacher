eea319be8f813fa0a3059e3a53827da31ae6252c
T0N1R <reytono@gmail.com>
2022-03-13 14:42:31 -0600

    feat: :sparkles: create Embed Message

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const {Client, Intents} = require("discord.js");
+const {Client, Intents, MessageEmbed} = require("discord.js");
 require("dotenv").config();
 
 const prefix = "$"
@@ -12,6 +12,43 @@ client.once('ready', (bot) => {
 
 client.login(process.env.DISCORD_TOKEN);
 
+// Embed Message
+const me = new MessageEmbed()
+    .setColor([0,255,255])
+    .setTitle("Welcome")
+    .setURL("https://youtu.be/wd2mV4KkAG4")
+    .setAuthor({ 
+        name: 'Emma Frost', 
+        iconURL: 'https://insidepulse.com/wp-content/uploads/2019/05/House-of-X-Powers-Of-X-X-Men-logo-symbol-e1558148793647.jpg',
+        url: "https://marvel.fandom.com/wiki/Emma_Frost_(Earth-616)?so=search"
+    }
+    )
+    .setDescription("Descripción del Embed Message")
+    .setThumbnail("https://geekd-out.com/wp-content/uploads/2021/10/inferno-2-feat.jpeg")
+    .addFields(
+        {
+            name: "Powers", 
+            value:"Emma welcomes you to Krakoa"
+        },
+        {
+            name: "Col 1",
+            value: "Columna 1 descripcion",
+            inline: true
+        },
+        {
+            name: "Col 2",
+            value: "Columna 2 descripcion",
+            inline: true
+        }
+    )
+    .setImage("https://cdn.vox-cdn.com/thumbor/5nAYzWf54TdyHPX3xQo7Ygbf1lY=/1400x0/filters:no_upscale()/cdn.vox-cdn.com/uploads/chorus_asset/file/22889116/white_queen_inferno.jpg")
+    .setTimestamp()
+    .setFooter({
+        text: "Emma Frost", 
+        iconURL: "https://geekd-out.com/wp-content/uploads/2021/10/inferno-2-feat.jpeg"
+    })
+
+
 // captar mensaje
 client.on("messageCreate", (msg) => {
 
@@ -24,7 +61,18 @@ client.on("messageCreate", (msg) => {
         
         const comando = argumentos.shift().toLowerCase();
 
-        if (comando == "ping") return msg.reply("pong");
+        const me2 = new MessageEmbed()
+            .setColor([138,50,252])
+            .setTitle(comando)
+            .setAuthor({
+                name: msg.author.username, 
+                iconURL: msg.author.displayAvatarURL(),
+            })
+            .setDescription(`El comando elegido es ${comando}`);
+
+        msg.channel.send({embeds: [me2] })
+
+        if (comando == "welcome") return msg.reply({embeds: [me]});
 
     }
 
