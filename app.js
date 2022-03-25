17217f6afea18beeb6cbb26e83023f85d1413eb1
T0N1R <reytono@gmail.com>
2022-03-25 00:52:06 -0600

    feat: :sparkles: test embed message in slash command & add documentation

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
-import { Client, Intents, MessageEmbed, Collection } from 'discord.js';
+import discord from 'discord.js';
 import sendReg from './embedMsg/sendReg.js'
 import dotenv from 'dotenv';
 import characters from './characters/characters.js'
+import welcomeMsg from './embedMsg/welcomeMsg.js';
 
 dotenv.config();
 
 const prefix = "$"
 
-const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+const client = new discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] });
 
 let slashCommands = [
     {
@@ -24,9 +25,17 @@ let slashCommands = [
             await interaction.followUp({ content: "Hola como estas?" });
         },
     },
+    {
+        name: "help",
+        description: "Obtener información del servidor y de los comandos",
+        run: async (client, interaction) => {
+            console.log(client);
+            await interaction.followUp({ embeds: [welcomeMsg] });
+        },
+    }
 ];
 
-client.slash = new Collection();
+client.slash = new discord.Collection();
 
 // codigo
 client.once('ready', async (bot) => {
@@ -37,54 +46,16 @@ client.once('ready', async (bot) => {
     client.user.setStatus('online');
     client.user.setActivity('Quiet Council', { type: "WATCHING" });
 
-    client.slash.set(slashCommands[0].name, slashCommands[0]);
-    client.slash.set(slashCommands[1].name, slashCommands[1]);
+    for (let index = 0; index < slashCommands.length; index++) {
+        client.slash.set(slashCommands[index].name, slashCommands[index]);
+    }
 
     await client.application.commands.set(slashCommands)
 
 });
 
 
-
 client.login(process.env.DISCORD_TOKEN);
-
-// first Embed Message
-// Embed Message
-const me = new MessageEmbed()
-    .setColor([0, 255, 255])
-    .setTitle("Welcome")
-    .setURL("https://youtu.be/wd2mV4KkAG4")
-    .setAuthor({
-        name: 'Emma Frost',
-        iconURL: 'https://insidepulse.com/wp-content/uploads/2019/05/House-of-X-Powers-Of-X-X-Men-logo-symbol-e1558148793647.jpg',
-        url: "https://marvel.fandom.com/wiki/Emma_Frost_(Earth-616)?so=search"
-    }
-    )
-    .setDescription("Descripción del Embed Message")
-    .setThumbnail("https://geekd-out.com/wp-content/uploads/2021/10/inferno-2-feat.jpeg")
-    .addFields(
-        {
-            name: "Powers",
-            value: "Emma welcomes you to Krakoa"
-        },
-        {
-            name: "Col 1",
-            value: "Columna 1 descripcion",
-            inline: true
-        },
-        {
-            name: "Col 2",
-            value: "Columna 2 descripcion",
-            inline: true
-        }
-    )
-    .setImage("https://cdn.vox-cdn.com/thumbor/5nAYzWf54TdyHPX3xQo7Ygbf1lY=/1400x0/filters:no_upscale()/cdn.vox-cdn.com/uploads/chorus_asset/file/22889116/white_queen_inferno.jpg")
-    .setTimestamp()
-    .setFooter({
-        text: "Emma Frost",
-        iconURL: "https://geekd-out.com/wp-content/uploads/2021/10/inferno-2-feat.jpeg"
-    })
-
 
 
 // https://ouroboros.world/sites/default/files/inline-images/IMG_20191002_100040.jpg
@@ -126,8 +97,6 @@ client.on("messageCreate", (msg) => {
 
         const comando = argumentos.shift().toLowerCase();
 
-        if (comando == "welcome") return msg.reply({ embeds: [me] });
-
         if (msg.channel.name == "quiet-council") {
 
             if (comando == "reg") {
@@ -138,7 +107,7 @@ client.on("messageCreate", (msg) => {
                     }]
 
                 let messageE = sendReg(characters.charles_xavier, messageFields);
-                msg.channel.send({ embeds: [messageE] });
+                msg.channel.reply({ embeds: [messageE] });
 
             }
 
